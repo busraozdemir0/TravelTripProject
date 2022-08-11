2461d211041cad26e34e074cce699068f2b5cf77
Büşra Özdemir <98536303+busraozdemir0@users.noreply.github.com>
2022-08-11 15:49:05 +0300

    30. Ders Sonu

--- TravelTripProject/Controllers/DefaultController.cs
+++ TravelTripProject/Controllers/DefaultController.cs
@@ -3,19 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using TravelTripProject.Models.Siniflar;
 namespace TravelTripProject.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.Take(7).ToList();
+            return View(degerler);
         }
         public ActionResult About()
         {
             return View();
         }
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial2()
+        {
+            var degerler = c.Blogs.Where(x => x.ID == 1).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial5()
+        {
+            var deger = c.Blogs.Take(3).OrderByDescending(x=>x.ID).ToList();
+            return PartialView(deger);
+        }
+
     }
 }
